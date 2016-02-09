437ba0f9f8cb7aa45be8d0792cd5a737af13b95a
Jordan Hand <jordanhand@comcast.net>
2016-02-09 01:31:13 -0800

    fixed stuff

--- ssh-vim-setup.sh
+++ ssh-vim-setup.sh
@@ -3,6 +3,6 @@ cp -r ./.vim/ ~
 cp ./.vim/vimrc ~/.vimrc
 cp ./.bashrc ~/.bashrc
 
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
