fd7ca6337e5f515c0006416812741a6d9b43ce0f
Jordan Hand <jorhand@microsoft.com>
2019-02-08 15:39:52 -0800

    Install vundle and plugins in setup script

--- ssh-vim-setup.sh
+++ ssh-vim-setup.sh
@@ -4,3 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null && pwd  )"
 cp $DIR/.vimrc ~/.vimrc
 cp $DIR/.bashrc ~/.bashrc
 cp $DIR/.tmux.conf ~/.tmux.conf
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install plugins
+vim -c PluginInstall -c :q -c :q
