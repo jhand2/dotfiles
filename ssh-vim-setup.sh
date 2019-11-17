d682f1769eae1f427c10462593879445a7b3f463
Jordan Hand <jordanhand22@gmail.com>
2019-11-16 17:10:01 -0800

    Update install script

--- ssh-vim-setup.sh
+++ ssh-vim-setup.sh
@@ -6,10 +6,14 @@ cp $DIR/.bashrc ~/.bashrc
 cp $DIR/.tmux.conf ~/.tmux.conf
 
 # Install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # Install plugins
 vim -c PluginInstall -c :q -c :q
