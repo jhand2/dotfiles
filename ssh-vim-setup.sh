6eb4ccf634a367a5fd684955d51b791d3416bd96
Jordan Hand <jorhand@microsoft.com>
2020-05-15 17:20:34 -0700

    Install some basic packages

--- ssh-vim-setup.sh
+++ ssh-vim-setup.sh
@@ -5,6 +5,16 @@ cp $DIR/.vimrc ~/.vimrc
 cp $DIR/.bashrc ~/.bashrc
 cp $DIR/.tmux.conf ~/.tmux.conf
 
+PACKAGES="vim-gnome tmux git"
+
+if hash apt-get 2>/dev/null; then
+    # Install packages on debian/ubuntu
+    sudo apt-get install -y $PACKAGES
+elif hash dnf 2>/dev/null
+    # Install packages on fedora/centos
+    sudo dnf install -y $PACKAGES
+fi
+
 # Install vundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -18,7 +28,3 @@ fi
 # Install plugins
 vim -c PluginInstall -c :q -c :q
 
-# What else should I install?
-# - vim
-# - tmux
-# - tmux plugin manager
