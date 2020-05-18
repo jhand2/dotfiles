7b3d03c47fb9bd5021197d52ff901cba9bbdb7db
Jordan Hand <jorhand@microsoft.com>
2020-05-18 15:40:35 -0700

    Finish up windows setup

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -3,6 +3,10 @@
 # run it directly, it is just to remember all the commands I need to make a
 # Windows VM.
 
+$email "me@me.me"
+$keyfile = "path/to/privkey"
+$user_profile = "C:\Users\me"
+
 # Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
@@ -20,3 +24,15 @@ choco install -y win32-openssh -params "/SSHServerFeature /KeyBasedAuthenticatio
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+
+# Generate RSA keypair for ssh
+ssh-keygen -t rsa -b 4096 -C $email
+Start-Service ssh-agent
+ssh-add $keyfile
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $user_profile\.vim\bundle\Vundle.vim
+
+# Set powershell as the default for ssh sessions
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
